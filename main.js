0887160548330e8e5a12c48c0145c618da91cb73
sohailcs140 <120193343+sohailcs140@users.noreply.github.com>
2024-11-17 22:40:38 +0500

    add mulitplication

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("hello github");
+const multiplication = (...args)=>{
+
+    ans = 1
+
+    args.forEach(elem=>{
+        ans *= elem
+    })
+
+    return ans
+}
+
+console.log(multiplication(10, 2));
